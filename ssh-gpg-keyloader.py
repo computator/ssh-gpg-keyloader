add67d61932d02df4b0cef4fbbfc2c053cc37ffa
Reuben Lifshay <rlifshay@gmail.com>
2020-06-13 01:14:21 -0700

    Minimal working code to load keys.

--- ssh-gpg-keyloader.py
+++ ssh-gpg-keyloader.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
+import glob
+import os.path
+import subprocess
 
 
 def cli():
-    pass
+    for keyfile in glob.iglob(os.path.expanduser('~/.ssh-keystore/*.key.gpg')):
+        try:
+            key = subprocess.run(
+                ['gpg2', '--quiet', '--batch', '--decrypt', keyfile],
+                check=True,
+                stdout=subprocess.PIPE
+            ).stdout
+            subprocess.run(
+                ['ssh-add', '-k', '-q', '-'],
+                input=key,
+                check=True
+            )
+        except subprocess.CalledProcessError as e:
+            print(e)
 
 
 if __name__ == '__main__':
