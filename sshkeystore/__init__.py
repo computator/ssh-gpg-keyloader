fe88ccbe4c947c1be8f2d1139291394c1a963b36
Reuben Lifshay <rlifshay@gmail.com>
2020-06-16 19:20:06 -0700

    Send errors to stderr.

--- sshkeystore/__init__.py
+++ sshkeystore/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from . import store
 from .agent import Agent, AgentError
 from .keys import KeyLoadError
@@ -8,18 +10,18 @@ def cli():
         pubstore = store.PubdirStore.get_default_store()
     except Exception as e:
         pubstore = None
-        print(f"Error getting PubdirStore: {e}")
+        print(f"Error getting PubdirStore: {e}", file=sys.stderr)
     for kp in store.Keystore.get_default_store():
         try:
             Agent.addkey(kp.private())
         except AgentError as e:
-            print(f"Error adding key '{kp.name}' to agent: {e}")
+            print(f"Error adding key '{kp.name}' to agent: {e}", file=sys.stderr)
         except KeyLoadError as e:
-            print(f"Error loading key '{kp.name}': {e}")
+            print(f"Error loading key '{kp.name}': {e}", file=sys.stderr)
         else:
             print(f"Added '{kp.name}'")
             try:
                 if pubstore:
                     pubstore.add(kp.name, kp.public())
             except KeyLoadError as e:
-                print(f"Error loading public key for '{kp.name}': {e}")
+                print(f"Error loading public key for '{kp.name}': {e}", file=sys.stderr)
